66c2f4ef84f298f3361ca5371d6232167d0da17b
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2023-11-02 13:11:46 -0700

    ID.me logo support

--- src/main/java/com/cognitivemedicine/demo/spruce/views/LoginView.java
+++ src/main/java/com/cognitivemedicine/demo/spruce/views/LoginView.java
@@ -71,9 +71,13 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
     {
         setSizeFull();
 
-        StreamResource resource = new StreamResource("logindotgov_logo.svg", ()->getClass().getResourceAsStream("/logindotgov_logo.svg"));
-        Image image = new Image(resource,"Login.gov");
-        image.setHeight("18px");
+        StreamResource resource_lg = new StreamResource("logindotgov_logo.svg", ()->getClass().getResourceAsStream("/logindotgov_logo.svg"));
+        Image image_lg = new Image(resource_lg,"Login.gov");
+        image_lg.setHeight("18px");
+
+        StreamResource resource_idme = new StreamResource("Primary-IDme-Logo-RGB-white.svg", ()->getClass().getResourceAsStream("/Primary-IDme-Logo-RGB-white.svg"));
+        Image image_idme = new Image(resource_idme,"Id me");
+        image_idme.setHeight("18px");
 
         VerticalLayout layout = new VerticalLayout();
         layout.setJustifyContentMode(JustifyContentMode.CENTER);
@@ -83,15 +87,16 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         String buttonWidth = "350px";
         //Create Login Buttons
         //Button loginDotGov = new Button("LOGON.GOV");
-        Button loginDotGov = new Button(image);
+        Button loginDotGov = new Button(image_lg);
         loginDotGov.setWidth(buttonWidth);
         loginDotGov.addClassNames("logindotgov");
         loginDotGov.setAriaLabel("Sign on with login.gov");
 
-        Button idMe = new Button("ID me");
+        Button idMe = new Button(image_idme);
         idMe.addClassName("idme");
         idMe.setWidth(buttonWidth);
         idMe.setAriaLabel("Sign on with ID me");
+
         Button dsLogon = new Button("DS Logon");
         dsLogon.addClassName("dslogon");
         dsLogon.setWidth(buttonWidth);
